9c50ef84872a247516322c0d4d3b9b3da1667eea
Cynadyde <cynadydeindustries@gmail.com>
2019-05-16 22:53:22 -0400

    init commit

---
+++ src/me/cynadyde/teleportals/TeleportalsPlugin.java
@@ -0,0 +1,17 @@
+package me.cynadyde.teleportals;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TeleportalsPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
