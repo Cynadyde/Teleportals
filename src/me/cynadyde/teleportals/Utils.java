f42f9d8c392ec53ce841cf410147e73895fb147f
Cynadyde <cynadydeindustries@gmail.com>
2019-05-19 00:33:28 -0400

    added rng

--- src/me/cynadyde/teleportals/Utils.java
+++ src/me/cynadyde/teleportals/Utils.java
@@ -2,13 +2,20 @@ package me.cynadyde.teleportals;
 
 import org.bukkit.ChatColor;
 
+import java.util.Random;
+
 /**
- *
+ * Utilities for the Teleportals plugin.
  */
 @SuppressWarnings("WeakerAccess")
 public class Utils {
 
-    public static String format(String message, Object...objects) {
+    public static final Random RNG = new Random();
+
+    /**
+     * translates chat colors to the message, then formats it.
+     */
+    public static String format(String message, Object... objects) {
 
         return String.format(ChatColor.translateAlternateColorCodes('&', message), objects);
     }
